e4d007aa4e7fcbed05a9057fb56851aeededd344
gimyoni <hie0908@naver.com>
2021-07-04 21:24:45 +0900

    이벤트 핸들링(onClick & onKeyUp)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const onSubmit = () =>{
+    alert('submitted');
+  };
+
+  const onKeyUp = (event) =>{
+    if(event.keyCode === '13'){
+      onSubmit();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp = {onKeyUp} />
+      <button onClick={onSubmit}>Submit</button>
     </div>
   );
 }
 
+ 
 export default App;
