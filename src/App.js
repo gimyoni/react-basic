643139961824bb3149aa270f7e930da1775e6d8a
gimyoni <hie0908@naver.com>
2021-07-05 23:04:10 +0900

    리액트 훅스 useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [text, setText] = useState('Hello');
 
   const onSubmit = () =>{
     alert('submitted');
@@ -12,10 +14,27 @@ function App() {
     }
   }
 
+  // let text = 'Hello';
+
+  const updateText = () =>{
+    // text = 'Bye';  
+    setText('Bye');
+    console.log(text);
+  }
+
+  console.log(text);
+
   return (
     <div className="App">
-      <input onKeyUp = {onKeyUp} />
+      <input onKeyUp={onKeyUp} />
       <button onClick={onSubmit}>Submit</button>
+
+
+      <br /><br />
+
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
+
     </div>
   );
 }
