32caca437c562773d3a8d9f56650c0fcba575603
gimyoni <hie0908@naver.com>
2021-07-09 22:09:13 +0900

    자식 컴포넌트에 데이터 Props로 보내기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Counter from './components/Counter';
 
 function App() {
 
+  const [buttonName, setButtonName] = useState('클릭');
+  const clickButton = () =>{
+    setButtonName('click');
+  };
+
   return (
     <div className="App">
       <h1>Kossie Coder</h1>
+      <Counter click ="click1"/>
+      <Counter click = {buttonName}/>
       <Counter />
-      <Counter />
-      <Counter />
+      <button onClick ={clickButton}>Click</button>
     </div>
   );
 }
