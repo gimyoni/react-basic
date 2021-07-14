ecd58cd098acec929438bde33812569140713b9d
gimyoni <hie0908@naver.com>
2021-07-14 21:14:43 +0900

    자식 컴포넌트에서 부모 컴포넌트 스테이트 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react";
 import Movie from './components/Movie';
+import MovieForm from './components/MovieForm';
 
 function App() {
   const [movieTitle, setMovieTitle] = useState('');
@@ -11,43 +12,21 @@ function App() {
     { title: 'kossie coder4', year: 2004},
    ]);
 
-  useEffect(()=>{
-    console.log('render');
-  });
-
   const renderMovies = movies.map(movie => {
     return (
       <Movie movie = {movie} key = {movie.title}/>
     );
   });
-  const addMovie = (event) =>{
-    event.preventDefault();
+  const addMovie = (movie) =>{
     setMovies([
       ...movies,
-      {
-      title: movieTitle,
-      year: movieYear,
-      }
+      movie
     ])
   };
   return (
     <div className="App">
       <h1>Movie List</h1>
-      <form onSubmit = {addMovie}>
-        <input
-          type = "text"
-          value = {movieTitle}
-          placeholder = "영화제목"
-          onChange = {e => setMovieTitle(e.target.value)}
-        /><br />
-        <input
-          type = "text"
-          value = {movieYear}
-          placeholder = "개봉년도"
-          onChange = {e => setMovieYear(e.target.value)}
-        /><br />
-        <button type ="submit">영화추가</button>
-      </form>
+      <MovieForm addMovie = {addMovie} />
       {renderMovies}
     </div>
   );
